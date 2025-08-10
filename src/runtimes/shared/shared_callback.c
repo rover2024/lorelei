ac65f6c4f5319e438524bd12f8ac4f2089bf0a0c
rover2024 <rover2024@foxmail.com>
2025-08-10 12:19:41 +0000

    wip

--- src/runtimes/shared/shared_callback.c
+++ src/runtimes/shared/shared_callback.c
@@ -55,7 +55,7 @@ static void generate_jump(void *buf, void *target) {
 #elif defined(__aarch64__)
 // Generate:
 //     Save current Entry to X16 and jump to address Entry-N
-static void generate_thunk(void *buf, size_t N) {
+static void generate_thunk(void *buf, int64_t N) {
     uint32_t *instr = (uint32_t *) buf;
 
     // _start:
@@ -91,29 +91,37 @@ static void generate_jump(void *buf, void *target) {
 #elif defined(__riscv)
 // Generate:
 //     Save current Entry to t1 and jump to address Entry-N
-static void generate_thunk(char *buf, size_t N) {
+static void generate_thunk(char *buf, int64_t N) {
     uint32_t *instr = (uint32_t *) buf;
-    int32_t imm = -(int32_t) N;
+    int32_t imm = -4 - (int32_t) N;
 
     // _start:
     // auipc t1, 0
     instr[0] = 0x00000317;
 
     // jal x0, offset
-    int32_t offset = (-4 - N) / 2;
-    instr[1] = 0x0000006F | ((offset & 0xFFFFF) << 12);
+    uint32_t imm20 = ((imm >> 20) & 0x1) << 31;
+    uint32_t imm10_1 = ((imm >> 1) & 0x3FF) << 21;
+    uint32_t imm11 = ((imm >> 11) & 0x1) << 20;
+    uint32_t imm19_12 = ((imm >> 12) & 0xFF) << 12;
+
+    uint32_t rd = 0;        // x0
+    uint32_t opcode = 0x6F; // JAL opcode
+    instr[1] = imm20 | imm19_12 | imm11 | imm10_1 | (rd << 7) | opcode;
 }
 
 // Generate:
 //     Jump to address Entry
 static void generate_jump(void *buf, void *target) {
     struct instr {
+        uint32_t ld_t1;
         uint32_t auipc_t0;
         uint32_t ld_t0_12;
         uint32_t jr_t0;
         uint64_t target_addr;
     } __attribute__((packed));
     struct instr *instr = (struct instr *) buf;
+    instr->ld_t1 = 0xFF833303;    // ld t1, -8(t1)
     instr->auipc_t0 = 0x00000297; // auipc t0, 0
     instr->ld_t0_12 = 0x00C2B283; // ld t0, 12(t0)
     instr->jr_t0 = 0x00028067;    // jr t0
